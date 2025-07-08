eb65446d5b3cfba00420808595517f6add16fa14
Shaurya <shaurya.35k@gmail.com>
2025-07-08 19:03:31 +0530

    Basic Data types and print

---
+++ Code.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+
+    printf("Hello World \n"); // here \n is used to crete a line.
+
+    int age = 18;
+    float pi = 3.14;
+    char hashtag = '#'; // here symbols like # are to be displayed with the help of ''.
+
+
+    printf("age is %d \n",age); //here %d is used which will be replaced by age and \n is used to create line. also %d is used for intergers
+
+    printf("value of pi is %f \n", pi); //%f is used for float variable
+ 
+
+
+    return 0;
+
+}
