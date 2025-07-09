f1a3683fecc93aa3228da3ce2784cae0ccb925f7
Shaurya <shaurya.35k@gmail.com>
2025-07-09 20:11:03 +0530

    Adding 2 files

---
+++ Addition.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+
+    int sum , x , y;
+
+   printf("Enter 1st value");
+   scanf("%d", &x );
+
+   printf("Enter 2nd number");
+   scanf("%d", &y );
+
+   sum = y + x;
+
+   printf(" The sum is : %d" , sum);
+
+   return 0;
+
+}
---
+++ Area_Square.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+
+    int side;
+
+    printf("Enter value of Side");
+    scanf("%d", &side);
+
+    int area = side * side;
+
+    printf("The area is : %d", area);
+
+}
+
+
+
